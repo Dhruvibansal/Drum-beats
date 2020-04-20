99e51dc753870af1425cae899667ef0366dcb9e5
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-04-20 18:29:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ document.querySelector("button").addEventListener("click", handleclick)
 function handleclick() {
     alert("i got clicked!");
 }
+
+addEventListener("keypress", function() {
+    alert("key was pressed")
+});
